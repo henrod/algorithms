7e968c9b2d22bde2baf6c34351af20afd6eeabf0
Henrique Rodrigues <henrique.rodrigues@wildlifestudios.com>
2022-11-05 13:22:31 -0300

    Add optimized longest common subsequence

--- dynamic_programming/dp_types/4_longest_common_subsequence/longest_common_subsequence/longest_common_subsequence.py
+++ dynamic_programming/dp_types/4_longest_common_subsequence/longest_common_subsequence/longest_common_subsequence.py
@@ -1,6 +1,22 @@
 '''
 https://leetcode.com/problems/longest-common-subsequence/
+
+Solution: https://youtu.be/Ua0GhsJSlWM
 '''
+
+
+def longest_common_subsequence_optimized(text1: str, text2: str) -> int:
+    dp: list[int] = [0 for _ in range(len(text2) + 1)]
+
+    for i in range(len(text1) - 1, -1, -1):
+        prev = 0
+        for j in range(len(text2) - 1, -1, -1):
+            if text1[i] == text2[j]:
+                prev, dp[j] = dp[j], prev + 1
+            else:
+                prev, dp[j] = dp[j], max(dp[j], dp[j + 1])
+
+    return dp[0]
 
 
 def longest_common_subsequence(text1: str, text2: str) -> int:
@@ -14,7 +30,7 @@ def longest_common_subsequence(text1: str, text2: str) -> int:
             if text1[i] == text2[j]:
                 dp[i][j] = dp[i+1][j+1] + 1
             else:
-                dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+                dp[i][j] = max(dp[i+1][j], dp[i][j + 1])
 
     return dp[0][0]
 
@@ -24,6 +40,7 @@ if __name__ == '__main__':
         ('abcde', 'ace', 3),
         ('abc', 'abc', 3),
         ('abc', 'def', 0),
+        ('abcba', 'abcbcba', 5),
     ]
 
     for n_test, (text1, text2, expected) in enumerate(tests):
