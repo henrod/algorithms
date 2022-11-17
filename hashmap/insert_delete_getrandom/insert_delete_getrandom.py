e39b46c7e7e519939be6971c7132a363763bc8fd
Henrique Rodrigues <henrique.rodrigues@wildlifestudios.com>
2022-11-16 22:13:09 -0300

    Accept duplicates in insert_delete_getrandom

--- hashmap/insert_delete_getrandom/insert_delete_getrandom.py
+++ hashmap/insert_delete_getrandom/insert_delete_getrandom.py
@@ -8,23 +8,26 @@ import random
 class ValuesHolder:
     def __init__(self) -> None:
         self._arr: list[int] = []
-        self._mapping: dict[int, int] = {}
+        self._mapping: dict[int, list[int]] = {}
 
     def insert(self, value: int) -> None:
         self._arr.append(value)
-        self._mapping[value] = len(self._arr) - 1
+        if value not in self._mapping:
+            self._mapping[value] = []
+
+        self._mapping[value].append(len(self._arr) - 1)
 
     def delete(self, value: int) -> None:
-        i = self._mapping[value]
+        i = self._mapping[value][-1]
         j = len(self._arr) - 1
 
         self._arr[i], self._arr[j] = self._arr[j], self._arr[i]
         self._arr.pop()
 
-        del self._mapping[value]
+        self._mapping[value].pop()
 
         if i != j:
-            self._mapping[self._arr[i]] = i
+            self._mapping[self._arr[i]].append(i)
 
     def get_random(self) -> int:
         i = random.randint(0, len(self._arr) - 1)
@@ -36,16 +39,17 @@ def run_tests():
         ('insert', 1),
         ('insert', 2),
         ('insert', 3),
+        ('insert', 3),
         ('random', {1: 0, 2: 0, 3: 0}),
         ('delete', 3),
-        ('random', {1: 0, 2: 0}),
+        ('random', {1: 0, 2: 0, 3: 0}),
         ('insert', 4),
-        ('random', {1: 0, 2: 0, 4: 0}),
+        ('random', {1: 0, 2: 0, 3: 0, 4: 0}),
     ]
 
     holder = ValuesHolder()
 
-    for op, value in enumerate(tests):
+    for op, value in tests:
         if op == 'insert':
             holder.insert(value)
         elif op == 'delete':
