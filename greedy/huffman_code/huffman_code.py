54c523d831e3c25f9d3aa9609c7b36ae466287b8
Henrique Rodrigues <henrique.rodrigues@wildlifestudios.com>
2022-11-17 09:51:22 -0300

    Add huffman code decode function

--- greedy/huffman_code/huffman_code.py
+++ greedy/huffman_code/huffman_code.py
@@ -13,77 +13,111 @@ class Node:
     def __lt__(self, other: Node) -> bool:
         return self.freq < other.freq
 
-
-def huffman_decode(word: str) -> int:
-    pass
-
-
-def huffman_encode(word: str) -> str:
-    nodes = _nodes(word)
-    root = _build_tree(nodes)
-    _set_code(root, [])
-
-    encoded = []
-    for letter in word:
-        encoded.append(nodes[letter].code)
-
-    return ''.join(encoded)
+    def __repr__(self) -> str:
+        return f'({self.letter}: {self.code}) {self.left}, {self.right}'
 
 
-def _build_tree(nodes: dict[str, Node]) -> Node:
-    heap = list(nodes.values())
-    heapq.heapify(heap)
+class HuffmanCode:
+    def __init__(self, word: str) -> None:
+        self.nodes = self._nodes(word)
+        self.root = self._build_tree(self.nodes)
+        self._set_code(self.root, [])
 
-    while len(heap) > 1:
-        node = Node('')
-        node.left = heapq.heappop(heap)
-        node.freq = node.left.freq
-        if heap:
+    def encode(self, word: str) -> str:
+        encoded = []
+        for letter in word:
+            encoded.append(self.nodes[letter].code)
+
+        return ''.join(encoded)
+
+    def decode(self, code: str) -> str:
+        word: list[str] = []
+        i = 0
+        while i < len(code):
+            i = self._decode(code, i, self.root, word)
+        return ''.join(word)
+
+    def _decode(self, code: str, i: int, node: Node, word: list[str]) -> int:
+        if node.letter:
+            word.append(node.letter)
+            return i
+
+        if i >= len(code):
+            return i
+
+        if code[i] == '0':
+            i = self._decode(code, i + 1, node.left, word)
+        else:
+            i = self._decode(code, i + 1, node.right, word)
+
+        return i
+
+    def _build_tree(self, nodes: dict[str, Node]) -> Node:
+        heap = list(nodes.values())
+        heapq.heapify(heap)
+
+        while heap:
+            node = Node('')
+            node.left = heapq.heappop(heap)
+            node.freq = node.left.freq
+            if not heap:
+                break
+
             node.right = heapq.heappop(heap)
             node.freq += node.right.freq
+            if not heap:
+                break
 
-        heapq.heappush(heap, node)
+            heapq.heappush(heap, node)
 
-    return node
+        return node
 
+    def _nodes(self, word: str) -> dict[str, Node]:
+        nodes: dict[str, Node] = {}
+        for letter in word:
+            if letter not in nodes:
+                nodes[letter] = Node(letter)
+            nodes[letter].freq += 1
+        return nodes
 
-def _nodes(word: str) -> dict[str, Node]:
-    nodes: dict[str, Node] = {}
-    for letter in word:
-        if letter not in nodes:
-            nodes[letter] = Node(letter)
-        nodes[letter].freq += 1
-    return nodes
+    def _set_code(self, node: Node | None, code: list[str]) -> None:
+        if not node:
+            return
 
+        if node.letter:
+            node.code = ''.join(code)
+            return
 
-def _set_code(node: Node | None, code: list[str]) -> None:
-    if not node:
-        return
+        code.append('0')
+        self._set_code(node.left, code)
+        code.pop()
 
-    if node.letter:
-        node.code = ''.join(code)
-        return
-
-    code.append('0')
-    _set_code(node.left, code)
-    code.pop()
-
-    code.append('1')
-    _set_code(node.right, code)
-    code.pop()
+        code.append('1')
+        self._set_code(node.right, code)
+        code.pop()
 
 
 def run_tests() -> None:
     tests = [
+        ('a', '0'),
         ('aaaabbc', '1111010100'),
         ('bcabaaa', '0100101111'),
     ]
 
     for n_test, (word, expected) in enumerate(tests):
-        result = huffman_encode(word)
+        huffman = HuffmanCode(word)
+
+        print(huffman.root)
+
+        result = huffman.encode(word)
         if result != expected:
-            raise Exception(f'Test #{n_test} failed:'
+            raise Exception(f'Test #{n_test} failed for encoding:'
                             f'result={result}, expected={expected}')
+
+        decoded = huffman.decode(result)
+        if word != decoded:
+            raise Exception(f'Test #{n_test} failed for decoding:'
+                            f'result={decoded}, expected={word}')
 
     print('Success!')
 
