c5e8343d6d908d8b4f827253db9c17cc36040d4a
Henrique Rodrigues <henrique.rodrigues@wildlifestudios.com>
2022-11-17 22:03:43 -0300

    Add radixsort

---
+++ sorting/radixsort/radixsort.py
@@ -0,0 +1,39 @@
+def radixsort(nums: list[int]) -> list[int]:
+    div, rem = 1, 10
+    max_num = max(nums)
+
+    while div <= max_num:
+        buckets: list[list[int]] = [[] for _ in range(10)]
+        for num in nums:
+            idx = num % rem // div
+            buckets[idx].append(num)
+
+        nums = []
+        for bucket in buckets:
+            nums.extend(bucket)
+
+        div *= 10
+        rem *= 10
+
+    return nums
+
+
+def run_tests() -> None:
+    tests = [
+        ([10, 1, 100, 10], [1, 10, 10, 100]),
+        ([4, 3, 2, 1], [1, 2, 3, 4]),
+        ([10, 2, 1003, 99], [2, 10, 99, 1003]),
+        ([560, 409, 422], [409, 422, 560]),
+    ]
+
+    for n_test, (nums, expected) in enumerate(tests):
+        result = radixsort(nums)
+        if result != expected:
+            raise Exception(f'Test #{n_test} failed:'
+                            f'result={result}, expected={expected}')
+
+    print('Success!')
+
+
+if __name__ == '__main__':
+    run_tests()
